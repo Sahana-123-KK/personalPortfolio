e3cbb0ee9ed1de186cb9e3f8cb9f7ff61c8c0aa2
Sahana-123-KK <105161293+Sahana-123-KK@users.noreply.github.com>
2022-11-16 12:59:28 +0530

    some fixes

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -18,6 +18,9 @@ const Projects = () => {
               Its a Portfolio website built on react
             </span>
             <img src={project} alt="" className="proimg" />
+            <a href="https://sahana-123-kk.github.io/portfolio/">
+              Here's my Work
+            </a>
             <span className="ptech">Technologies Used: HTML,CSS,React Js</span>
           </div>
           <div className="pro">
@@ -27,6 +30,9 @@ const Projects = () => {
               and like posts
             </span>
             <img src={project} alt="" className="proimg" />
+            <a href="https://github.com/Sahana-123-KK/Memories-App">
+              Here's my Work
+            </a>
             <span className="ptech">
               Technologies Used: HTML,CSS,Bootstrap,MERN
             </span>
@@ -38,6 +44,9 @@ const Projects = () => {
               account,login,logout,create,update,delete and view their notes
             </span>
             <img src={project} alt="" className="proimg" />
+            <a href="https://github.com/Sahana-123-KK/INotebook-auth">
+              Here's my Work
+            </a>
             <span className="ptech">
               Technologies Used: HTML,CSS,Bootstrap,MERN
             </span>
@@ -51,6 +60,9 @@ const Projects = () => {
               user.
             </span>
             <img src={project} alt="" className="proimg" />
+            <a href="https://github.com/Sahana-123-KK/Hotel-Booking-App">
+              Here's my Work
+            </a>
             <span className="ptech">
               Technologies Used: HTML,CSS,Bootstrap,MERN
             </span>
@@ -59,12 +71,18 @@ const Projects = () => {
             <h2 className="headpro">Restaurant Webiste</h2>
             <span className="pdesc">Restaurent Landing Page</span>
             <img src={project} alt="" className="proimg" />
+            <a href="My Restaurent.com (sahana-123-kk.github.io)">
+              Here's my Work
+            </a>
             <span className="ptech">Technologies Used: HTML,CSS</span>
           </div>
           <div className="pro">
             <h2 className="headpro">College Website</h2>
             <span className="pdesc">College Website</span>
             <img src={project} alt="" className="proimg" />
+            <a href="https://sahana-123-kk.github.io/Admin-Page-IIITDM/">
+              Here's my Work
+            </a>
             <span className="ptech">Technologies Used: HTML,CSS,Bootstrap</span>
           </div>
           <div className="pro">
@@ -74,6 +92,9 @@ const Projects = () => {
               groups, and can send and read messages of fellow groupmates
             </span>
             <img src={project} alt="" className="proimg" />
+            <a href="https://github.com/Sahana-123-KK/chat-app">
+              Here's my Work
+            </a>
             <span className="ptech">
               Technologies Used: HTML,CSS,JavaScript,Socket.io,Node Js
             </span>
@@ -84,8 +105,38 @@ const Projects = () => {
               Music Player-Where users can play,pause,play next,previous music
             </span>
             <img src={project} alt="" className="proimg" />
+            <a href="https://sahana-123-kk.github.io/iSpotify-A-Music-App/">
+              Here's my Work
+            </a>
             <span className="ptech">
               Technologies Used: HTML,CSS,JavaScript
+            </span>
+          </div>
+          <div className="pro">
+            <h2 className="headpro">i-Weather</h2>
+            <span className="pdesc">
+              React Application where temperature of any city can be found
+            </span>
+            <img src={project} alt="" className="proimg" />
+            <a href="https://sahana-123-kk.github.io/iWeather/">
+              Here's my Work
+            </a>
+            <span className="ptech">
+              Technologies Used: HTML,CSS,JavaScript,React Js
+            </span>
+          </div>
+          <div className="pro">
+            <h2 className="headpro">Todo List</h2>
+            <span className="pdesc">React Application,Todo List</span>
+            <img src={project} alt="" className="proimg" />
+            <a
+              href="https://sahana-123-kk.github.io/Todo-List/
+"
+            >
+              Here's my Work
+            </a>
+            <span className="ptech">
+              Technologies Used: HTML,CSS,JavaScript,React Js
             </span>
           </div>
         </div>
